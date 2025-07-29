d1aefdd825880adfe366b558b75555f8f863051f
Xi Bai <baixiac@gmail.com>
2025-07-29 17:48:36 +0100

    feat: add the endpoint compatible with OpenAI client protocols

--- tests/app/api/test_serving_hf_llm.py
+++ tests/app/api/test_serving_hf_llm.py
@@ -1,8 +1,10 @@
 import httpx
+import json
 import pytest
 import app.api.globals as cms_globals
 
 from unittest.mock import create_autospec
+from fastapi.testclient import TestClient
 from app.api.api import get_generative_server
 from app.model_services.huggingface_llm_model import HuggingFaceLlmModel
 from app.utils import get_settings
@@ -27,14 +29,72 @@ def llm_app(llm_model_service):
     yield app
     app.dependency_overrides.clear()
 
+@pytest.fixture(scope="function")
+def client(llm_model_service):
+    llm_model_service.generate.return_value = "Yeah."
+    app = get_generative_server(config, msd_overwritten=lambda: llm_model_service)
+    app.dependency_overrides[cms_globals.props.current_active_user] = lambda: None
+    client = TestClient(app)
+    yield client
+    client.app.dependency_overrides.clear()
+
+
+def test_generate(client):
+    response = client.post(
+        "/generate?max_tokens=128&temperature=0.7",
+        data="Alright?",
+        headers={"Content-Type": "text/plain"},
+    )
+
+    assert response.status_code == 200
+    assert response.headers["x-cms-tracking-id"], "x-cms-tracking-id header is missing"
+    assert response.headers["content-type"] == "text/plain; charset=utf-8"
+    assert response.text == "Yeah."
+
 
 @pytest.mark.asyncio
 async def test_stream_generate(llm_model_service, llm_app):
+    llm_model_service.generate_async.return_value = "Fine."
     async with httpx.AsyncClient(app=llm_app, base_url="http://test") as ac:
         response = await ac.post(
-            "/stream/generate?max_tokens=32",
+            "/stream/generate?max_tokens=32&temperature=0.7",
             data="How are you doing?",
             headers={"Content-Type": "text/plain"},
         )
 
     assert response.status_code == 200
+    assert response.headers["x-cms-tracking-id"], "x-cms-tracking-id header is missing"
+    assert response.headers["content-type"] == "text/event-stream; charset=utf-8"
+    assert response.text == "Fine."
+
+
+@pytest.mark.asyncio
+async def test_generate_chat_completions(llm_model_service, llm_app):
+    llm_model_service.generate.return_value = "I'm a chat bot."
+    request_data = {
+      "messages": [
+        {
+          "role": "system",
+          "content": "You are a chat bot."
+        },
+        {
+          "role": "user",
+          "content": "Who are you?"
+        }
+      ],
+      "stream": True,
+      "max_tokens": 128,
+      "temperature": 0.7
+    }
+    async with httpx.AsyncClient(app=llm_app, base_url="http://test") as ac:
+        response = await ac.post(
+            "/v1/chat/completions?max_tokens=128&temperature=0.7",
+            data=json.dumps(request_data),
+            headers={"Content-Type": "application/json"},
+        )
+
+    assert response.status_code == 200
+    assert response.headers["content-type"] == "text/event-stream; charset=utf-8"
+    assert response.text.startswith("data:")
+    assert "id" in response.text
+    assert "chat.completion.chunk" in response.text
